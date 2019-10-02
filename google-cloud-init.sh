4ccbcf6ac3cafce35c81fc3f7cbc6996b21dcbdf
Abdulla Abdurakhmanov <abdulla.abd.m@gmail.com>
2019-10-02 17:03:30 +0300

    GOOGLE_PROJECT_ZONE support

--- google-cloud-init.sh
+++ google-cloud-init.sh
@@ -6,6 +6,7 @@
 #   GOOGLE_KEY_FILE
 #   GOOGLE_PROJECT
 #   GOOGLE_PROJECT_REGION
+#   GOOGLE_PROJECT_ZONE
 #   GKE_CLUSTER_NAME
 # Optional globals:
 #   DEBUG
@@ -17,6 +18,7 @@ enable_debug
 GOOGLE_KEY_FILE=${GOOGLE_KEY_FILE:?'GOOGLE_KEY_FILE variable missing.'}
 GOOGLE_PROJECT=${GOOGLE_PROJECT:?'GOOGLE_PROJECT variable missing.'}
 GOOGLE_PROJECT_REGION=${GOOGLE_PROJECT_REGION:?'GOOGLE_PROJECT_REGION variable missing.'}
+GOOGLE_PROJECT_ZONE=${GOOGLE_PROJECT_ZONE:?'GOOGLE_PROJECT_ZONE variable missing.'}
 GKE_CLUSTER_NAME=${GKE_CLUSTER_NAME:?'GKE_CLUSTER_NAME variable missing.'}
 
 info "Setting up GCP environment".
@@ -27,6 +29,6 @@ export GOOGLE_APPLICATION_CREDENTIALS=/tmp/key-file.json
 
 run gcloud config set project ${GOOGLE_PROJECT} --quiet ${gcloud_debug_args}
 run gcloud config set compute/${GOOGLE_PROJECT_REGION} --quiet ${gcloud_debug_args}
-run gcloud config set compute/zone ${GOOGLE_PROJECT_REGION} --quiet ${gcloud_debug_args}
-run gcloud container clusters get-credentials ${GKE_CLUSTER_NAME} --zone ${GOOGLE_PROJECT_REGION} --project ${GOOGLE_PROJECT} --quiet ${gcloud_debug_args}
+run gcloud config set compute/zone ${GOOGLE_PROJECT_ZONE} --quiet ${gcloud_debug_args}
+run gcloud container clusters get-credentials ${GKE_CLUSTER_NAME} --zone ${GOOGLE_PROJECT_ZONE} --project ${GOOGLE_PROJECT} --quiet ${gcloud_debug_args}
 run gcloud auth configure-docker --quiet ${gcloud_debug_args}
